e87df8ee3e38476cef8290689e704fcb328840e5
jayne.lsgama <jayneelopesg23@gmail.com>
2024-04-26 22:06:16 -0300

    Detalhes

---
+++ app/src/main/java/com/example/atividade001/CartApiService.kt
@@ -0,0 +1,13 @@
+package com.example.atividade001
+import retrofit2.Call
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CartApiService {
+    @GET("/getCartItems")
+    fun getCartItems(@Query("userId") userId: Int): Call<List<Produto>>
+
+    @DELETE("/deleteCartItem")
+    fun deleteCartItem(@Query("produtoId") produtoId: Int, @Query("userId") userId: Int): Call<Void>
+}
--- app/src/main/java/com/example/atividade001/ProdutosDetalhesActivity.kt
+++ app/src/main/java/com/example/atividade001/ProdutosDetalhesActivity.kt
@@ -1,11 +1,12 @@
-
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.atividade001.CartActivity
 import com.example.atividade001.R
 import retrofit2.Call
 import retrofit2.Callback
@@ -24,9 +25,8 @@ class ProdutoDetalhesActivity : AppCompatActivity() {
 
         val nomeProduto = intent.getStringExtra("NOME_PRODUTO") ?: "Nome nÃ£o disponÃ­vel"
         val descricaoProduto = intent.getStringExtra("DESCRICAO_PRODUTO") ?: "DescriÃ§Ã£o nÃ£o disponÃ­vel"
-        val produtoId = intent.getIntExtra("PRODUTO_ID", 0)
+        val produtoId = intent.getIntExtra("ID_PRODUTO", 0)
         val quantidadeDisponivel = intent.getIntExtra("QUANTIDADE_DISPONIVEL", 0)
-
 
         findViewById<TextView>(R.id.txtNomeProduto).text = nomeProduto
         findViewById<TextView>(R.id.txtDescricaoProduto).text = descricaoProduto
@@ -35,17 +35,24 @@ class ProdutoDetalhesActivity : AppCompatActivity() {
         val editTextQuantidade = findViewById<EditText>(R.id.editQuantidadeDesejada)
         val btnAdicionarCarrinho = findViewById<Button>(R.id.btnAdicionarAoCarrinho)
 
+
         val sharedPreferences = getSharedPreferences("Login", Context.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
-        val variavelComId = 0
-        editor.putInt("userId", variavelComId)
+        val userId = sharedPreferences.getInt("userId", 0)
         editor.apply()
 
         btnAdicionarCarrinho.setOnClickListener {
             val quantidadeDesejada = editTextQuantidade.text.toString().toIntOrNull() ?: 0
-            val userId = 0
             adicionarAoCarrinho(userId, produtoId, quantidadeDesejada)
         }
+    }
+
+    //Esse a gente que procura
+        btnCarrinho.setOnClickListener {
+            val intent = Intent(this, CartActivity::class.java)
+            intent.putExtra("userId", userId)
+            startActivity(intent)
+            finish()
     }
 
     private fun adicionarAoCarrinho(userId: Int, produtoId: Int, quantidade: Int) {

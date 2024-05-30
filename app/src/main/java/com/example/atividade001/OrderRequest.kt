4ee815dbdcc34fc329d2f52690a19659ff54443b
jayne.lsgama <jayneelopesg23@gmail.com>
2024-05-29 21:51:51 -0300

    Final

--- app/src/main/java/com/example/atividade001/OrderRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.example.atividade001
-
-data class OrderRequest(
-        val userId: Int,
-        val total: Double,
-        val products: List<Produto>
-    )
-
--- app/src/main/java/com/example/atividade001/ResponseCompra.kt
+++
@@ -1,8 +0,0 @@
-package com.example.atividade001
-
-
-data class ResponseCompra(
-    val status: String,
-    val code: Int,
-    val message: String
-)
